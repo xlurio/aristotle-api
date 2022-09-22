25a8a9f60ce2248f07e8cbe1746e878b8b713da9
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-22 12:37:11 -0300

    feat: developing authorization system

--- app/absence_register/api/views.py
+++ app/absence_register/api/views.py
@@ -1,8 +1,12 @@
-from core import viewsets, models
+from core import viewsets
 from absence_register.api.serializers import AbsenceSerializer
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class AbsenceViewSet(viewsets.WriteOnlyViewSet):
     """Write only end point for registering absences"""
 
     serializer_class = AbsenceSerializer
+    permission_classes = [DjangoModelPermissions]
+    authentication_classes = [TokenAuthentication]
--- app/grade_register/api/views.py
+++ app/grade_register/api/views.py
@@ -1,8 +1,12 @@
 from core import viewsets
 from grade_register.api.serializers import GradeSerializer
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class GradeViewSet(viewsets.WriteOnlyViewSet):
     """Write only end point for registering grades"""
 
     serializer_class = GradeSerializer
+    permission_classes = [DjangoModelPermissions]
+    authentication_classes = [TokenAuthentication]
